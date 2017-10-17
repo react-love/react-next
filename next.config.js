ad072731116ebecba6f9f2ae552e4ecb08e79488
yongyuehuang <1130216245@qq.com>
2017-10-17 10:28:17 +0800

    增加preact-compat

--- next.config.js
+++ next.config.js
@@ -27,6 +27,14 @@ module.exports = {
                 ]
             }
         )
+        if (dev) {
+            return config
+        }
+    
+        config.resolve.alias = {
+            'react': 'preact-compat/dist/preact-compat',
+            'react-dom': 'preact-compat/dist/preact-compat'
+        }
         return config
     }
 }
--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 const express = require('express')
 const next = require('next')
 const compression = require('compression')
+const LRUCache = require('lru-cache')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+// 缓存设置
+const ssrCache = new LRUCache({
+    max: 100,
+    maxAge: 1000 * 60 * 60 // 1hour
+})
+
 app.prepare()
 .then(() => {
     const server = express()
     server.use(compression()) //gzip
     
+    server.get('/', (req, res) => {
+        renderAndCache(req, res, '/')
+    })
+    
     server.get('/search', (req, res) => {
         return app.render(req, res, '/search', req.query)
     })
+    
+    //server.get('/book/:id', (req, res) => {
+    //    const queryParams = { id: req.params.id }
+    //    renderAndCache(req, res, '/book', queryParams)
+    //})
     
     server.get('*', (req, res) => {
         return handle(req, res)
@@ -25,3 +41,26 @@ app.prepare()
         console.log(`> Ready on http://localhost:${port}`)
     })
 })
+function getCacheKey (req) {
+    return `${req.url}`
+}
+function renderAndCache (req, res, pagePath, queryParams) {
+    const key = getCacheKey(req)
+    // 存在缓存
+    if (ssrCache.has(key)) {
+        console.log(`CACHE HIT: ${key}`)
+        res.send(ssrCache.get(key))
+        return
+    }
+    // 无缓存，重新渲染
+    app.renderToHTML(req, res, pagePath, queryParams)
+    .then((html) => {
+        // 缓存页面
+        console.log(`CACHE MISS: ${key}`)
+        ssrCache.set(key, html)
+        res.send(html)
+    })
+    .catch((err) => {
+        app.renderError(err, req, res, pagePath, queryParams)
+    })
+}
