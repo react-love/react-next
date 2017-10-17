08b522b5de113d0058833a58901653fe6bc05c92
yongyuehuang <1130216245@qq.com>
2017-10-17 10:49:49 +0800

    preact出现bug，删除

--- next.config.js
+++ next.config.js
@@ -27,14 +27,6 @@ module.exports = {
                 ]
             }
         )
-        if (dev) {
-            return config
-        }
-    
-        config.resolve.alias = {
-            'react': 'preact-compat/dist/preact-compat',
-            'react-dom': 'preact-compat/dist/preact-compat'
-        }
         return config
     }
 }
